7ca6c8e7b74dccae2d52a7a2583acb46710ab52d
carlos anguix <carlos.anguix@gmail.com>
2019-02-17 18:59:01 +0100

    nuevos atributos producto

---
+++ src/almacen/Almacen.java
@@ -0,0 +1,11 @@
+package almacen;
+
+import java.util.ArrayList;
+
+import productos.Producto;
+
+public class Almacen {
+
+	private ArrayList<Producto> listaDeProductos;
+	
+}
---
+++ src/productos/Producto.java
@@ -0,0 +1,11 @@
+package productos;
+
+public class Producto {
+
+	private String codigo;
+	private String nombre;
+	private int    cantidad;
+	private double precioCompra;
+	private double precioVenta;
+	
+}
